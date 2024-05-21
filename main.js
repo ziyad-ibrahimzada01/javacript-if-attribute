e05f54eddc89d4ce65906c9f53d79825a69c4d32
Zahid Ibrahimov <zahid.ibrahimov@mlspp.gov.az>
2024-05-22 00:29:27 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,80 @@
+// let week = prompt('enter the number 1-7')
+// if(week==1){
+//     alert('monday')
+// }
+// else if(week==2){
+//     alert('tuesday')
+// }
+// else if(week==3){
+//     alert('wednesday')
+// }
+// else if(week==4){
+//     alert('thursday')
+// }
+// else if(week==5){
+//     alert('friday')
+// }
+// else if(week==6){
+//     alert('saturday')
+// }
+// else if(week==7){
+//     alert('sunday')
+// }
+// else{
+//     alert('not found')
+// }
+
+
+
+
+// let weather=confirm('wanna see rainy weather?')
+// if(weather){
+//     alert('rainy weather')
+// }
+// else{
+//     alert('sunnny weather')
+// }
+
+
+
+
+// let scholarship =prompt('enter your age')
+// let position =confirm('are you jobless?')
+// if(scholarship>50 && position){
+//     alert('full pension')
+// }
+// else if(scholarship==50 || position){
+//     alert('half of pension')
+// }
+// else{
+//     alert('rejected')
+// }
+
+
+
+
+// let gpa = prompt('enter your gpa 0-100')
+// let point = prompt('enter your point A-D')
+// if(gpa>90 && point==='A'){
+//     alert('passed exam')
+// }
+// else if(80<gpa<91 && point==='B'){
+//     alert('passed to the next stage')
+// }
+// else if(70<gpa<81 && point==='C'){
+//     alert('apply again')
+// }
+// else if(61<gpa<71 && point==='D'){
+//     alert('failed exam')
+// }
+// else{
+//     alert('failed exam')
+// }
+
+
+
+
+// let number = prompt('enter any number that can be rooted')
+// if(number){
+//     alert(number**0.5)
+// }
